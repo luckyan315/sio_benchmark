349e8609d6d8672dc6725698e60d3fcb2fcaff60
luckyan315 <lucky315.an@gmail.com>
2014-05-14 18:28:39 +0800

    fix opts bug

--- test/sio_benchmark.test.js
+++ test/sio_benchmark.test.js
@@ -4,16 +4,22 @@ require('should');
 var ioc = require('socket.io-client');
 var debug = require('debug')('benchmark:test');
 var Server = require('./support/server.js');
-var sb = require('../sio_benchmark.js');
+var sb = require('../sio_benchmark.js')({
+  // follow args format of `minimist`
+  _: ['http://localhost:'],
+  n: 100,
+  c: 1000
+});
 
 // sio benchmark
 describe('sio benchmark ', function(){
   var sio = Server({});  
   before(function(){
-
+    
   });
 
   it('should connect to the sio', function(done){
+    sb.run();
     done();
   });  
 });
