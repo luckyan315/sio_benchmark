e59a089603d9b5935a0c94822f84340072db8943
luckyan315 <luckyan315@qq.com>
2014-05-14 21:54:53 +0800

    up tests

--- test/sio_benchmark.test.js
+++ test/sio_benchmark.test.js
@@ -4,21 +4,23 @@ require('should');
 var ioc = require('socket.io-client');
 var debug = require('debug')('benchmark:test');
 var Server = require('./support/server.js');
-var sb = require('../sio_benchmark.js')({
-  // follow args format of `minimist`
-  _: ['http://localhost:'],
-  n: 100,
-  c: 1000
-});
+var parseArgs = require('minimist');
+
+var siobenchmark = require('../sio_benchmark.js');
+var port = 3000;
 
 // sio benchmark
 describe('sio benchmark ', function(){
-  var sio = Server({});  
+  var sio = Server();
+  
   before(function(){
-    
+    sio.listen(port, function(){
+      debug('sio server listening port on ' + port);
+    });
   });
 
   it('should connect to the sio', function(done){
+    var sb = siobenchmark(parseArgs('-c 100 -n 1000 http://localhost:3000/user'.split(' ')));
     sb.run();
     done();
   });  
