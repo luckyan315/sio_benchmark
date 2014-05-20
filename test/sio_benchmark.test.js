4d3f2124c2b8acd6c08c565f9c5e76a189e1c8bf
luckyan315 <lucky315.an@gmail.com>
2014-05-20 15:23:59 +0800

    more tests(check -c concurrency)

--- test/sio_benchmark.test.js
+++ test/sio_benchmark.test.js
@@ -5,7 +5,7 @@ var ioc = require('socket.io-client');
 var debug = require('debug')('benchmark:test');
 var Server = require('./support/server.js');
 var parseArgs = require('minimist');
-
+var _ = require('lodash');
 var benchmark = require('../');
 var port = 3000;
 
@@ -15,17 +15,25 @@ describe('sio benchmark ', function(){
   
   before(function(){
     sio.listen(port, function(){
-      debug('sio server listening port on ' + port);
+      debug('sio server listening port on %d', port);
     });
   });
 
-  xit('should create workers instants by -c param', function(done){
-
+  it('should create workers instants by -c param', function(done){
+    var mockArgs = parseArgs('-n 10 -c 2 --ioc 10 http://localhost:3000/user'.split(' '));
+    var expWorkers = 2;
+    var nb = benchmark(mockArgs);
+    nb.on('all connected', function(nClients){
+      expWorkers.should.eql(_.size(nb.cluster.workers));
+      done();
+    })
+    nb.run();
   });
 
   it('should create ioc instants by --ioc param', function(done){
     var expClients = 10;
-    var nb = benchmark(parseArgs('-n 10 -c 1 --ioc 10 http://localhost:3000/user'.split(' ')));
+    var mockArgs = parseArgs('-n 10 -c 1 --ioc 10 http://localhost:3000/user'.split(' '));
+    var nb = benchmark(mockArgs);
     nb.on('all connected', function(nClients){
       expClients.should.eql(nClients);
       done();
