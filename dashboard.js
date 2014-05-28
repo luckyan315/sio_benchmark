16013de692539245200c218ff65ce3482a03864c
luckyan315 <lucky315.an@gmail.com>
2014-05-28 17:08:26 +0800

    pack query

--- dashboard.js
+++ dashboard.js
@@ -9,6 +9,9 @@
 var express = require('express');
 var app = express();
 var debug = require('debug')('benchmark:express');
+var sprintf = require('sprintf');
+var parseArgs = require('minimist');
+var benchmark = require('./');
 
 var http_port = 6666;
 
@@ -21,11 +24,15 @@ app.get('/start', function(req, res, next){
   var nConcurrency = req.query.c;
   var nClientsPerWorker = req.query.ioc;
   var nIntervalTime = req.query.t;
+  
   if(!nRequests || !nConcurrency || !nClientsPerWorker || !nIntervalTime){
     res.writeHead(500);
     res.end('\x1b[1;31mPlease pass all params\x1b[m\n' + JSON.stringify(req.query));
     return;
   }
+
+  var args = packQuery(nRequests, nConcurrency, nClientsPerWorker, nIntervalTime);
+  debug('[args] %j', args);
 
   res.send('start!!!!\n');
 });
@@ -34,3 +41,7 @@ app.get('/start', function(req, res, next){
 app.listen(http_port, function(){
   debug('Express server listening port on %d ...', http_port);
 });
+
+function packQuery(n, c, ioc, t){
+  return parseArgs(sprintf('-n %s -c %s --ioc %s -t %s', n, c, ioc, t).split(' '));
+};
