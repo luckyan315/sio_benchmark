73f2dfc44c9c86772edad660783398373a29efd5
luckyan315 <lucky315.an@gmail.com>
2014-05-28 16:43:07 +0800

    parse query

--- dashboard.js
+++ dashboard.js
@@ -13,8 +13,20 @@ var debug = require('debug')('benchmark:express');
 var http_port = 6666;
 
 app.get('/', function(req, res, next){
-  res.send('hell world');
+  res.send('Welcome to dashboard \n');
 });
+
+app.get('/start', function(req, res, next){
+  debug('[query] %j', req.query);
+  var nRequests = req.query.n;
+  var nConcurrency = req.query.c;
+  var nClientsPerWorker = req.query.ioc
+
+  debug('n %d, c %d, ioc %d', nRequests, nConcurrency, nClientsPerWorker);
+
+  res.send('start!!!!\n');
+});
+
 
 app.listen(http_port, function(){
   debug('Express server listening port on %d ...', http_port);
