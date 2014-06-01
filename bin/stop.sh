0e8de55346e5f1b7a07538dfee87fbc87b2f8c50
luckyan315 <luckyan315@qq.com>
2014-06-01 22:42:20 +0800

    mv scripts to bin

---
+++ bin/stop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# slaver servers
+slaver_local="localhost"
+
+# dashboard http listen port 
+dash_port=6666
+
+# action api
+stop="/stop"
+
+# close all connectioned clients in sio_benchmark clusters
+stop() {
+  echo -n "stop $1 ..."
+  slaver=$1
+  if [ -z $slaver ]; then
+    echo "Please pass slaver name! e.g.: stop 192.168.20.203:3000"
+    exit 1
+  fi
+
+  # pack cmd
+  cmd=$(printf "http://%s:%s%s" "$slaver" "$dash_port" "$stop")
+  curl $cmd
+}
+
+# stop remote sio_benchmark apps
+stop $slaver_local &
